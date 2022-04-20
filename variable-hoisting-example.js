b197cd44cc9d5a78c2ac2ae3991740d2a28ba857
RiazK <RiazCKhan@gmail.com>
2022-04-20 16:37:07 -0400

    hoisting example with function expression

---
+++ variable-hoisting-example.js
@@ -0,0 +1,36 @@
+// ----- Variable Hoisting Example -----
+
+// Pre Compile
+var a = 123;
+var b = 'abc';
+
+var fnExpression = function() {
+    var c = 456;
+    var d = 'def';
+}
+
+function fnDeclaration() {
+    var e = 789;
+}
+
+// Execution of Code
+function fnDeclaration() {
+  var e;
+
+  e = 789;
+}
+
+var a;
+var b;
+var fnExpression;
+
+a = 123;
+b = 'abc';
+
+fnExpression = function() {
+  var c;
+  var d;
+
+  c = 456;
+  d = 'def';
+}
