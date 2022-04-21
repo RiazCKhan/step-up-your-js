214015b2abdefe88c2e90fa9667fd026635f2597
RiazK <RiazCKhan@gmail.com>
2022-04-21 17:04:29 -0400

    inital commit

---
+++ value-vs-reference.js
@@ -0,0 +1,19 @@
+function changeAgeAndReference(person) {
+  person.age = 25;
+  person = {
+    name: 'John',
+    age: 50
+  };
+
+  return person;
+}
+
+const personObj1 = {
+  name: 'Alex',
+  age: 30
+};
+
+const personObj2 = changeAgeAndReference(personObj1);
+
+console.log(personObj1); // -> ?
+console.log(personObj2); // -> ?
