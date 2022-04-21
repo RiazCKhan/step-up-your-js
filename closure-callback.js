43d05fcfc8a4f5b8e06cccdd7eeeb7dbff451b1b
RiazK <RiazCKhan@gmail.com>
2022-04-21 16:09:37 -0400

    inital commit

---
+++ closure-callback.js
@@ -0,0 +1,10 @@
+// Make this function take in a parameter.
+// Make this function return a function.
+// Make the returned function return whatever was
+// originally passed in to func when it was called.
+
+function func(param) {
+  
+  return innerFN() 
+
+}
