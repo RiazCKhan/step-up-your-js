323383c45f795a3d5e9e80718dd39979f89b30a8
RiazK <RiazCKhan@gmail.com>
2022-04-21 17:09:27 -0400

    Update answer with notes

--- value-vs-reference.js
+++ value-vs-reference.js
@@ -18,5 +18,7 @@ const personObj1 = {
 
 const personObj2 = changeAgeAndReference(personObj1);
 
-console.log(personObj1); // -> Alex
-console.log(personObj2); // -> John
+console.log(personObj1); // -> { name: 'Alex', age: 25 }
+// Note the FN changes the value of age in the personObj1 
+// THEN personObj1 is reassigned to John
+console.log(personObj2); // -> { name: 'John', age: 50 }
