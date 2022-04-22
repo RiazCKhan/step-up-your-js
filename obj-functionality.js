9ed4199309bd007cfa12d9b8cdcb1fa735220b97
RiazK <RiazCKhan@gmail.com>
2022-04-22 10:27:18 -0400

    inital commit

---
+++ obj-functionality.js
@@ -0,0 +1,18 @@
+// Change this getter method such that
+// when we attempt to access the property
+// 'val', it multiplies by 2 every time.
+
+// Ex:
+/*
+console.log(obj.val); -> 2
+console.log(obj.val); -> 4
+console.log(obj.val); -> 8
+console.log(obj.val); -> 16
+*/
+
+const obj = {
+  _val: 1,
+  get val() {
+      // Your code here
+  }
+};
