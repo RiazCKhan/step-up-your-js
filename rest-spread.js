b3a3a5a9e44cf5fb693ad1513e36f2d89b5ff6f3
RiazK <RiazCKhan@gmail.com>
2022-04-25 11:08:53 -0400

    complete ex2 with notes

--- rest-spread.js
+++ rest-spread.js
@@ -22,6 +22,9 @@ const obj = {
   }
 };
 
+
+// ----- Exercise 1 -----
+
 // Shorten the long method chain in this function.
 // You might want to use arrow function and
 // new Object functions.
@@ -38,7 +41,7 @@ const sum = Object.keys(obj)
   .filter(item => item.cost > 0)
   .map(item => item.cost * item.amount)
   .reduce((sum, next) => sum + next);
-
+  
 
 // ------ ORIGINAL -----
 // const sum = Object.keys(obj)
@@ -51,3 +54,19 @@ const sum = Object.keys(obj)
 //     }).reduce(function(sum, next) {
 //       return sum + next;
 //     }); 
+
+
+// ----- Exercise 2 -----
+
+// Write the shortest function you possibly can
+// that takes in an array and returns a new array
+// that is identical to the original array,
+// but a different reference. In other words,
+// create an array shallow-copy function.
+
+const shallowCopy = (arr) => [ ...arr ]
+const org = [1, 2, 3, 4]
+const copy = shallowCopy([1, 2, 3, 4])
+console.log('this is false -> ', org === copy)
+// this is because the new arr points to a different point in memory, i.e., different reference
+
