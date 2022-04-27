f7555348a23dafa81a37514af085a034d5500256
RiazK <RiazCKhan@gmail.com>
2022-04-27 17:50:24 -0400

    inital commit

---
+++ this-arrow-function.js
@@ -0,0 +1,9 @@
+// Arrow Function Rule of THIS
+/* 
+If the function is an ES2015 arrow function, 
+it ignores all the rules above and receives the this value 
+of its surrounding scope at the time it’s created. 
+To determine what this is, go one line above the arrow function’s creation 
+and see what the value of this is there. 
+It will be the same in the arrow function.
+*/
