137e8a89ca487f7a86e1b144246f5b16d7891daf
RiazK <RiazCKhan@gmail.com>
2022-04-27 16:50:13 -0400

    Add rule 1 example

--- this-rules.js
+++ this-rules.js
@@ -1,3 +1,13 @@
 // RULE 1
 // If the new keyword is used when calling the function, this inside 
 // the function is a brand new object created by the JavaScript engine.
+
+function ConstructorExample() {
+  console.log(this);
+  this.value = 10;
+  console.log(this);
+}
+
+new ConstructorExample();
+// -> ConstructorExample {}
+// -> ConstructorExample { value: 10 }
