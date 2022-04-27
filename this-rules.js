d8ef8e938e939f04833d5e4190b7b39535b17e6e
RiazK <RiazCKhan@gmail.com>
2022-04-27 16:53:40 -0400

    rule 2 example

--- this-rules.js
+++ this-rules.js
@@ -16,3 +16,16 @@ new ConstructorExample();
 // If apply, call, or bind are used to call a function, this 
 // inside the function is the object that is passed in as the argument.
 
+function fn() {
+  console.log(this);
+}
+
+var obj = {
+  value: 5
+};
+
+var boundFn = fn.bind(obj);
+
+boundFn(); // -> { value: 5 }
+fn.call(obj); // -> { value: 5 }
+fn.apply(obj); // -> { value: 5 }
