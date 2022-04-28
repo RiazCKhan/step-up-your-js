5b2524b407302e10d64e3df92db3e5e70f3456ee
RiazK <RiazCKhan@gmail.com>
2022-04-28 19:10:38 -0400

    q4 inital commite

--- proto-inheritance.js
+++ proto-inheritance.js
@@ -16,10 +16,15 @@
 // Null = Null
 
 // 3 - What will print? True or False?
-function Fn() {}
-const obj = new Fn();
-console.log(obj.__proto__ === Object.prototype);
+// function Fn() {}
+// const obj = new Fn();
+// console.log(obj.__proto__ === Object.prototype);
 
 // Answer = False
 // console.log(obj.__proto__) // --> !== null
 // console.log(Object.prototype) // --> === null 
+
+// 4 - What will print? True or False?
+function Fn() {}
+const obj = new Fn();
+console.log(obj.__proto__ === Fn.prototype);
