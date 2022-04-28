5d79321647b267293181bc2ac53d3bc732ae1c0b
RiazK <RiazCKhan@gmail.com>
2022-04-28 18:58:46 -0400

    add answer and notes

--- proto-inheritance.js
+++ proto-inheritance.js
@@ -3,3 +3,7 @@
 // 1 - What will print? True or False?
 const obj = {};
 console.log(obj.__proto__ === Object.__proto__);
+
+// Answer = False
+// console.log(obj.__proto__) --> Null does not exist on the object literal
+// console.log(Object.__proto__) --> prints the empty obj
