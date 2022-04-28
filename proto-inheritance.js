ff5367cea96b65b944495014f9323a46e3c9a33c
RiazK <RiazCKhan@gmail.com>
2022-04-28 18:59:42 -0400

    question 2 inital commit

--- proto-inheritance.js
+++ proto-inheritance.js
@@ -1,9 +1,13 @@
 // ----- Quiz -----
 
 // 1 - What will print? True or False?
-const obj = {};
-console.log(obj.__proto__ === Object.__proto__);
+// const obj = {};
+// console.log(obj.__proto__ === Object.__proto__);
 
 // Answer = False
 // console.log(obj.__proto__) --> Null does not exist on the object literal
 // console.log(Object.__proto__) --> prints the empty obj
+
+// 2 - What will print? True or False?
+const obj = {};
+console.log(obj.__proto__ === Object.prototype);
