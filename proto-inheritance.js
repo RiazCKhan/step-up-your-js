e6198904eea85e313da004e8a89b4ea38dfd3caf
RiazK <RiazCKhan@gmail.com>
2022-04-28 19:12:50 -0400

    add q5 + formatting for readability

--- proto-inheritance.js
+++ proto-inheritance.js
@@ -1,6 +1,6 @@
 // ----- Quiz -----
 
-// 1 - What will print? True or False?
+// ----- 1 - What will print? True or False?
 // const obj = {};
 // console.log(obj.__proto__ === Object.__proto__);
 
@@ -8,14 +8,14 @@
 // console.log(obj.__proto__) --> Null does not exist on the object literal
 // console.log(Object.__proto__) --> prints the empty obj
 
-// 2 - What will print? True or False?
+// ----- 2 - What will print? True or False?
 // const obj = {};
 // console.log(obj.__proto__ === Object.prototype);
 
 // Answer = True
 // Null = Null
 
-// 3 - What will print? True or False?
+// ----- 3 - What will print? True or False?
 // function Fn() {}
 // const obj = new Fn();
 // console.log(obj.__proto__ === Object.prototype);
@@ -24,9 +24,11 @@
 // console.log(obj.__proto__) // --> !== null
 // console.log(Object.prototype) // --> === null 
 
-// 4 - What will print? True or False?
+// ----- 4 - What will print? True or False?
 function Fn() {}
 const obj = new Fn();
 console.log(obj.__proto__ === Fn.prototype);
 
 // Answer = True
+
+// ----- 5 - What will print? True or False?
