186818f7ae02bd8b02ef996f6dc206f48ab68fc7
RiazK <RiazCKhan@gmail.com>
2022-05-05 12:00:26 -0400

    Fixed function count with IIEF

--- IIEF.js
+++ IIEF.js
@@ -1,6 +1,10 @@
 // IIFE -> immediately invoked function expression
 
 for (var i = 0; i < 5; i++) { // We are explicitly using `var` for a reason
-  const time = i * 1000;
-  setTimeout(function() {console.log(i);}, time);
+  (function(num){
+
+    const time = num * 1000;
+    setTimeout(function() {console.log(num);}, time);
+
+  })(i)
 } // -> ?
