7eda7709bee0c3c9f5cb0df2f1d243a20bd3ca20
RiazK <RiazCKhan@gmail.com>
2022-05-05 14:39:59 -0400

    exercise 2 complete evenOdd FN

--- map-filter-practice.js
+++ map-filter-practice.js
@@ -10,4 +10,19 @@ const data = ['Hello', 'there!', 'How', 'are', 'you', 'doing?']
 
 const toUpperCase = data.map((item, index, arr) => arr[index][0].toUpperCase() + item.substring(1));
 
-console.log(toUpperCase)
+// console.log(toUpperCase)
+
+const data2 = [1, 2, 3, 4, 5]
+
+function evenOdd(item, index, arr) {
+
+  if (item % 2 === 0) {
+   return arr[index] = 'even'
+  }
+  else {
+    return arr[index] = 'odd'
+  }
+}
+
+console.log(data2.map(evenOdd))
+
