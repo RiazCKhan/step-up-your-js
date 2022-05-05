d609c11f61ecdd3ccc6b26dcf8f33b5cd0aa1c7c
RiazK <RiazCKhan@gmail.com>
2022-05-05 12:10:02 -0400

    Add examples of IIFE syntax

--- IIEF.js
+++ IIEF.js
@@ -2,9 +2,16 @@
 
 for (var i = 0; i < 5; i++) { // We are explicitly using `var` for a reason
   (function(num){
-
     const time = num * 1000;
     setTimeout(function() {console.log(num);}, time);
-
-  })(i)
+  })(i) // inovke function with 'i' as arguement.
 } // -> ?
+
+// EXAMPLES of IIFE
+
+(function(){})();
+(function(){}());
+!function(){}();
+~function(){}();
++function(){}();
+-function(){}();
