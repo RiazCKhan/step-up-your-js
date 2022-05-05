9682a46073fde7fd3e0c27a9840253a1beb84b19
RiazK <RiazCKhan@gmail.com>
2022-05-05 16:33:35 -0400

    complete sum fn with reduce and refactored

--- forEach-reduce-practice.js
+++ forEach-reduce-practice.js
@@ -1,7 +1,9 @@
 // Sum Arr
 
 function add(numbers) {
-  return numbers.reduce((accum, currentValue, currentIndex, array) => accum + currentIndex)
+  return numbers.reduce((accum, currentValue, currentIndex, array) => {
+    return accum + currentIndex
+  }, 0) // inital value necessary to start at 0
 }
 
 console.log(add([1, 2, 3, 4, 5, 6]))
