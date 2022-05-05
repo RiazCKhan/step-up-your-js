586b118cdb0844c6e3ef2d424e1d1c78ba79eafe
RiazK <RiazCKhan@gmail.com>
2022-05-05 14:13:07 -0400

    question 1 solved

--- map-filter-practice.js
+++ map-filter-practice.js
@@ -2,10 +2,8 @@
 
 const data = ['Hello', 'there!', 'How', 'are', 'you', 'doing?']
 
-function upperCase(item, index) {
-  
-  
-
+function upperCase(item, index, arr) {
+  return arr[index][0].toUpperCase() + item.substring(1)
 }
 
-data.map(upperCase)
+console.log(data.map(upperCase))
