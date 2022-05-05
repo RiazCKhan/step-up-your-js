6cebbd6953a4b67e5437c77e4017b38a10114234
RiazK <RiazCKhan@gmail.com>
2022-05-05 14:17:25 -0400

    refactor solution 1

--- map-filter-practice.js
+++ map-filter-practice.js
@@ -2,8 +2,12 @@
 
 const data = ['Hello', 'there!', 'How', 'are', 'you', 'doing?']
 
-function upperCase(item, index, arr) {
-  return arr[index][0].toUpperCase() + item.substring(1)
-}
+// function upperCase(item, index, arr) {
+//   return arr[index][0].toUpperCase() + item.substring(1)
+// }
 
-console.log(data.map(upperCase))
+// data.map(item, index, arr => arr[index][0].toUpperCase() + item.substring(1))
+
+const toUpperCase = data.map((item, index, arr) => arr[index][0].toUpperCase() + item.substring(1));
+
+console.log(toUpperCase)
