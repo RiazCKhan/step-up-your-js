cb30041fd6cf1ee6d1bed41f0d3ae94f1d0d1ed5
RiazK <RiazCKhan@gmail.com>
2022-05-06 13:30:27 -0400

    complete sumList FN for shopping list

--- objectKeys-practice.js
+++ objectKeys-practice.js
@@ -14,4 +14,34 @@ const sum = function (obj) {
   return value
 }
 
-console.log(sum(object))
+sum(object)
+
+const object2 = {
+  groceries: {
+    cost: 33.22,
+    amount: 1
+  },
+  rent: {
+    cost: 899.00,
+    amount: 1,
+  },
+  paycheck: {
+    cost: -2000,
+    amount: 2
+  },
+  restaurantBills: {
+    cost: 20,
+    amount: 4
+  }
+};
+
+const sumList = (obj) => {
+  let total = Object.keys(obj)
+    .map((key) => obj[key])
+    .filter((item) => item.cost > 0)
+    .map((item) => item.cost * item.amount)
+    .reduce((accum, currentValue) => accum + currentValue)
+  return total
+}
+
+console.log(sumList(object2))
